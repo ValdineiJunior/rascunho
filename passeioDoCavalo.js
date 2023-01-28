3f75a3d744f003dd7a22453ca477ea3c2566a262
Valdinei de Paula Junior <valdineidepaulajunior@gmail.com>
2023-01-28 11:58:54 -0300

    try solve knigth tour

--- passeioDoCavalo.js
+++ passeioDoCavalo.js
@@ -1,10 +1,10 @@
-let casa = [4,4]
-let result = [[4,4]]
-// let numbers = [8,7,6,5,4,3,2,1]
+let casa = [1,1]
+let result = [[1,1]]
+let numbers = [5,4,3,2,1]
 // let line = []
 // for (let i = 0; i < numbers.length; i++) {
 //   const elemento = numbers[i];
-//   for (let j = 7; j >= 0; j--) {
+//   for (let j = 4; j >= 0; j--) {
 //     const elementos = numbers[j];
 //     line.push([elemento,elementos])
 //   }
@@ -20,19 +20,15 @@ let xadrez = [
   ['21', '22', '23', '24', '25', '26', '27', '28'],
   ['11', '12', '13', '14', '15', '16', '17', '18']]
 let possibilidades = [
-  [ 8, 1 ], [ 8, 2 ], [ 8, 3 ], [ 8, 4 ], [ 8, 5 ],
-  [ 8, 6 ], [ 8, 7 ], [ 8, 8 ], [ 7, 1 ], [ 7, 2 ],
-  [ 7, 3 ], [ 7, 4 ], [ 7, 5 ], [ 7, 6 ], [ 7, 7 ],
-  [ 7, 8 ], [ 6, 1 ], [ 6, 2 ], [ 6, 3 ], [ 6, 4 ],
-  [ 6, 5 ], [ 6, 6 ], [ 6, 7 ], [ 6, 8 ], [ 5, 1 ],
-  [ 5, 2 ], [ 5, 3 ], [ 5, 4 ], [ 5, 5 ], [ 5, 6 ],
-  [ 5, 7 ], [ 5, 8 ], [ 4, 1 ], [ 4, 2 ], [ 4, 3 ],
-   [ 4, 5 ], [ 4, 6 ], [ 4, 7 ], [ 4, 8 ],
-  [ 3, 1 ], [ 3, 2 ], [ 3, 3 ], [ 3, 4 ], [ 3, 5 ],
-  [ 3, 6 ], [ 3, 7 ], [ 3, 8 ], [ 2, 1 ], [ 2, 2 ],
-  [ 2, 3 ], [ 2, 4 ], [ 2, 5 ], [ 2, 6 ], [ 2, 7 ],
-  [ 2, 8 ], [ 1, 1 ], [ 1, 2 ], [ 1, 3 ], [ 1, 4 ],
-  [ 1, 5 ], [ 1, 6 ], [ 1, 7 ], [ 1, 8 ]
+  [ 5, 1 ], [ 5, 2 ], [ 5, 3 ],
+  [ 5, 4 ], [ 5, 5 ], [ 4, 1 ],
+  [ 4, 2 ], [ 4, 3 ], [ 4, 4 ],
+  [ 4, 5 ], [ 3, 1 ], [ 3, 2 ],
+  [ 3, 3 ], [ 3, 4 ], [ 3, 5 ],
+  [ 2, 1 ], [ 2, 3 ],
+  [ 2, 4 ], [ 2, 5 ], [ 1, 1 ],
+  [ 1, 2 ], [ 1, 3 ], [ 1, 4 ],
+  [ 1, 5 ]
 ]
 
 function passeioDoCavalo(casa) {
@@ -40,10 +36,9 @@ function passeioDoCavalo(casa) {
   let posicaoinicial = casa.split('')
   posicaoinicial[0] = (referenciaCasas.findIndex((elemenet) => elemenet === posicaoinicial[0]) + 1).toString()
 }
-let jogadas = [[2,1],[2,-1],[1,2],[1,-2],[-1,2],[-1,-2],[-2,1],[-2,-1]];
-let lastMoveFind = 3
 
-function jogada(casa, jogada, possibilidades) {
+
+function verificaSeAJogadaEValida(casa, jogada, possibilidades) {
   let novaCasa = [casa[0]+jogada[0],casa[1]+jogada[1]]
   let index = possibilidades.findIndex((element) => element[0] == novaCasa[0] && element[1] == novaCasa[1])
   if (index != -1) {
@@ -53,55 +48,31 @@ function jogada(casa, jogada, possibilidades) {
   }
 }
 
-let count = 0
-function movimentos(casa, jogadas, possibilidades, result) {
-  let novaCasa = [...casa]
-  let novasJogadas = [...jogadas]
-  let novasPossibilidades = [...possibilidades]
-  let novoResult = [...result]
-  for (let index = 0; index < novasJogadas.length; index++) {
-    const existeUmaProximaJogada = novasJogadas[index];
-    novaCasa = jogada(novaCasa, existeUmaProximaJogada, novasPossibilidades);
-    let casaEIgualNovaCasa = casa[0] == novaCasa[0] && casa[1] == novaCasa[1]
-    if (!casaEIgualNovaCasa) {
-      let corte = novasPossibilidades.findIndex((element) => element[0] == novaCasa[0] && element[1] == novaCasa[1])
-      novoResult.push(novasPossibilidades[corte])
-      novasPossibilidades[corte] = novasPossibilidades.pop()
-      
-    }
-  }
-  // movimentos(casa, jogadas, possibilidades, result)
-  return novoResult;
+function movimentos(novaCasa, possibilidades, result) { 
+      let corte = possibilidades.findIndex((element) => element[0] == novaCasa[0] && element[1] == novaCasa[1])
+      result.push(possibilidades[corte])
+      if (corte < possibilidades.length-1) {
+        possibilidades[corte] = possibilidades.pop() 
+      } else {
+        possibilidades.pop()
+      }
 }
 
 
 passeioDoCavalo('c2')
-console.log(movimentos(casa, jogadas, possibilidades, result))
 
-
-// function verifyWord(array, input, responseArray) {
-//   let newArray = [...array];
-//   let newInput = [...input];
-//   const newresponseArray = [...responseArray];
-//   for (let index = 0; index < newArray.length; index++) {
-//       const word = newArray[index];
-//       const wordFitInTheNewInput = (cutTheInput(newInput, word)) !== newInput;
-//       if (wordFitInTheNewInput) {
-//           const arrayHasMoreThanTwoElements = newArray.length > 2;
-//           if (arrayHasMoreThanTwoElements) {
-//               const arrayWithoutCurrentElementToSearchForOtherCombinations = newArray.slice(index + 1);
-//               verifyWord(arrayWithoutCurrentElementToSearchForOtherCombinations, newInput, newresponseArray);
-//           }
-//           newInput = cutTheInput(newInput, word);
-//           newresponseArray.push(word);
-//           const inputIsEmpty = newInput.length === 0;
-//           if (inputIsEmpty) {
-//               console.log(newresponseArray.join(" "));
-//           } else {
-//               newArray = newArray.slice(index + 1);
-//               verifyWord(newArray, newInput, newresponseArray);
-//           }
-//           break;
-//       }
-//   }
-// }
+let jogadas = [[2,1],[2,-1],[1,2],[1,-2],[-1,2],[-1,-2],[-2,1],[-2,-1]];
+for (let index = 0; index < jogadas.length; index++) {
+  const jogada = jogadas[index];
+  let novaCasa = verificaSeAJogadaEValida(casa, jogada, possibilidades);
+  let casaEIgualNovaCasa = casa[0] == novaCasa[0] && casa[1] == novaCasa[1]
+  console.log(casaEIgualNovaCasa)
+  if (!casaEIgualNovaCasa) {
+    console.table(result)
+    movimentos(novaCasa, possibilidades, result)
+    console.table(result)
+  }
+  console.log(result)
+}
+console.log(result)
+movimentos(casa, possibilidades, result)
