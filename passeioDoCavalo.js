3d854e10098be13a5f45bee4c6e1175acbb540f0
Valdinei de Paula Junior <valdineidepaulajunior@gmail.com>
2023-01-31 17:20:16 -0300

    tentando aplicar conceito de busca de arvore binaria no algoritimo

--- passeioDoCavalo.js
+++ passeioDoCavalo.js
@@ -1,5 +1,6 @@
-const readlineSync = require("readline-sync");
-const chessNotation = readlineSync.question("Digite uma casa em notacao algebrica de xadrez que sera a posicao inicial do cavalo: ");
+// const readlineSync = require("readline-sync");
+// const chessNotation = readlineSync.question("Digite uma casa em notacao algebrica de xadrez que sera a posicao inicial do cavalo: ");
+const chessNotation = "a1"
 function passeioDoCavalo(chessNotation) {
   const referenciaCasas = ['a','b','c','d','e','f','g','h']
   let posicaoinicial = chessNotation.split('')
@@ -12,11 +13,11 @@ let casa = passeioDoCavalo(chessNotation)
 let result = [passeioDoCavalo(chessNotation)]
 
 function montarTabuleiro() {
-  let numbers = [6,5,4,3,2,1]
+  let numbers = [1,2,3,4,5]
   let line = []
   for (let i = 0; i < numbers.length; i++) {
     const elemento = numbers[i];
-    for (let j = 5; j >= 0; j--) {
+    for (let j = 0; j <= 4; j++) {
       const elementos = numbers[j];
       line.push([elemento,elementos])
     }
@@ -24,16 +25,38 @@ function montarTabuleiro() {
   return line
 }
 let possibilidades = montarTabuleiro()
-
+console.table(possibilidades)
 let indexFirstHouse = possibilidades.findIndex((element) => element[0] == casa[0] && element[1] == casa[1])
 possibilidades.splice(indexFirstHouse,1)
 function verificaSeAJogadaEValida(casa, jogada, possibilidades) {
+  let existeONumero = false
   let novaCasa = [casa[0]+jogada[0],casa[1]+jogada[1]]
-  let index = possibilidades.findIndex((element) => element[0] == novaCasa[0] && element[1] == novaCasa[1])
-  if (index != -1) {
-    return novaCasa
+  let min = 0
+  let max = possibilidades.length-1
+  let resto = (min + max)%2
+  let chute = ((min + max - resto)/2)
+  console.table(possibilidades)
+  console.table(novaCasa)
+  console.log(possibilidades[chute][0],novaCasa[0],possibilidades[chute][1],novaCasa[1])
+  if (possibilidades[chute][0] == novaCasa[0] && possibilidades[chute][1] == novaCasa[1]) {
+    existeONumero = true
   } else {
-    return casa
+    if (((novaCasa[0]*10) + novaCasa[1]) < (possibilidades[chute][0]*10) + possibilidades[chute][1]) {
+      max = chute - 1
+      console.log('max')
+      if (max < min) {
+        return existeONumero
+      } else {
+        verificaSeAJogadaEValida(casa, jogada, possibilidades.slice(min,max))
+      }
+    } else {
+      min = chute + 1
+      if (possibilidades.length > 1) {
+        verificaSeAJogadaEValida(casa, jogada, possibilidades.slice(min,max))
+      } else {
+        return existeONumero
+      }
+    }
   }
 }
 
@@ -58,9 +81,16 @@ function novoMovimento(casa, possibilidades) {
   let jogadas = [[2,1],[2,-1],[1,2],[1,-2],[-1,2],[-1,-2],[-2,1],[-2,-1]];
   for (let index = 0; index < jogadas.length; index++) {
     const jogada = jogadas[index];
-    let novaCasa = verificaSeAJogadaEValida(casa, jogada, possibilidades);
-    let casaEIgualNovaCasa = casa[0] == novaCasa[0] && casa[1] == novaCasa[1]
-    if (!casaEIgualNovaCasa) {
+    let novaCasa = [casa[0]+jogada[0],casa[1]+jogada[1]]
+    let novaCasaEValida = false
+    console.log
+    console.log(novaCasa[0] < 5 && novaCasa[0] > 1 &&novaCasa[1] < 5 && novaCasa[1] > 1)
+    if (novaCasa[0] < 5 && novaCasa[0] > 1 &&novaCasa[1] < 5 && novaCasa[1] > 1) {
+      novaCasaEValida = verificaSeAJogadaEValida(casa, jogada, possibilidades);
+    } else {
+      novaCasaEValida = false
+    }
+    if (novaCasaEValida) {
       movimentos(novaCasa, possibilidades, result)
       novoMovimento(novaCasa, possibilidades, result)
       if (possibilidades.length > 0) {
