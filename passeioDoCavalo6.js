2c2214172657c37d1d75e838c649c8896a314c0e
Valdinei de Paula Junior <valdineidepaulajunior@gmail.com>
2023-03-11 17:50:54 -0300

    versao final passeio do cavalo

--- passeioDoCavalo6.js
+++ passeioDoCavalo6.js
@@ -1,76 +1,100 @@
-const notacaoInicial = "a1";
-const casa = converteNotacaoInicialEmNumero(notacaoInicial);
-const result = [];
-const tamanhoTabuleiro = 8
+const file = process.argv[2];
+const notacaoInicial = file;
+const casaInicial = converteNotacaoInicialEmNumero(notacaoInicial);
+const resultado = [];
+const tamanhoTabuleiro = 8;
 const possibilidades = montarPossibilidadesDoTabuleiro(tamanhoTabuleiro);
-const possibilidadesBordaTabuleiro = coletarBordasDoTabuleiro(possibilidades, tamanhoTabuleiro)
-const possibilidadesSegundaCasaApartirDaBordaTabuleiro = coletarSegundaCasaApartirDaBordaDoTabuleiro(possibilidades, tamanhoTabuleiro)
-console.table(possibilidades)
-movimentos(casa, possibilidades, result);
-novoMovimento(casa, possibilidades, result);
-console.table(result);
+
+contabilizandoMovimento(casaInicial, possibilidades, resultado);
+passeioDoCavalo(casaInicial, possibilidades, resultado);
+
+console.table(resultado);
+for (let index = 0; index < resultado.length; index++) {
+    const notacaoNumerica = resultado[index];
+    console.log(converteNotacaoInicialEmNumero(notacaoNumerica));
+}
 
 function converteNotacaoInicialEmNumero(notacaoInicial) {
-    const referenciaCasas = ["a", "b", "c", "d", "e", "f", "g", "h"];
-    const posicaoinicial = notacaoInicial.split("");
-    posicaoinicial[0] = (
-        referenciaCasas.findIndex((elemenet) => elemenet === posicaoinicial[0]) + 1
-    );
-    posicaoinicial[1] = parseInt(posicaoinicial[1]);
-    return posicaoinicial;
+    const letrasDeReferenciaDasCasas = ["a", "b", "c", "d", "e", "f", "g", "h"];
+    if (typeof notacaoInicial === "string") {
+        const notacaoEmNumeros = notacaoInicial.split("");
+        notacaoEmNumeros[0] = (
+            letrasDeReferenciaDasCasas.findIndex((elemenet) => elemenet === notacaoEmNumeros[0]) + 1
+        );
+        notacaoEmNumeros[1] = parseInt(notacaoEmNumeros[1]);
+        return notacaoEmNumeros;
+    } else {
+        notacaoInicial[0] = letrasDeReferenciaDasCasas[notacaoInicial[0] - 1];
+        notacaoInicial[1] = notacaoInicial[1].toString();
+        const notacaoEmString = notacaoInicial[0] + notacaoInicial[1];
+        return notacaoEmString;
+    }
 }
 
 function montarPossibilidadesDoTabuleiro(tamanho) {
-    const numbers = [];
+    const colunas = [];
     for (let index = tamanho; index > 0; index--) {
-        numbers.push(index);
+        colunas.push(index);
     }
-    const tabuleiro = [];
-    for (let i = 0; i < numbers.length; i++) {
-        const linha = numbers[i];
-        for (let j = numbers.length - 1; j >= 0; j--) {
-            const coluna = numbers[j];
-            tabuleiro.push([linha, coluna]);
+    const possibilidadesDoTabuleiro = [];
+    for (let i = 0; i < colunas.length; i++) {
+        const linha = colunas[i];
+        for (let j = colunas.length - 1; j >= 0; j--) {
+            const coluna = colunas[j];
+            possibilidadesDoTabuleiro.push([linha, coluna]);
         }
     }
-    return tabuleiro;
+    return possibilidadesDoTabuleiro;
 }
 
-function movimentos(novaCasa, possibilidades, result) {
-    const corte = possibilidades.findIndex(
+function coletarBordasDoTabuleiro(possibilidades, tamanhoTabuleiro, casasApartirDaBorda) {
+    const casasPertencentesABorda = [];
+    for (let index = 0; index < possibilidades.length; index++) {
+        const casas = possibilidades[index];
+        const coletandoDiagonalSuperior = casas[0] <= 2 - casasApartirDaBorda | casas[1] <= 2 - casasApartirDaBorda;
+        const coletandoDiagonalInferior = casas[0] > tamanhoTabuleiro - casasApartirDaBorda | casas[1] > tamanhoTabuleiro - casasApartirDaBorda;
+        if (coletandoDiagonalSuperior | coletandoDiagonalInferior) {
+            casasPertencentesABorda.push(casas);
+        }
+    }
+    return casasPertencentesABorda;
+}
+
+function contabilizandoMovimento(novaCasa, possibilidades, result) {
+    const casaASerMovimentada = possibilidades.findIndex(
         (element) => element[0] === novaCasa[0] && element[1] === novaCasa[1]
     );
-    result.push(possibilidades[corte]);
-    if (corte < possibilidades.length - 1) {
-        possibilidades[corte] = possibilidades.pop();
+    result.push(possibilidades[casaASerMovimentada]);
+    if (casaASerMovimentada < possibilidades.length - 1) {
+        possibilidades[casaASerMovimentada] = possibilidades.pop();
     } else {
         possibilidades.pop();
     }
 }
 
-function movimentosReverso(novaCasa, possibilidades, result) {
-    possibilidades.push(result.pop());
-    novaCasa = result[result.length - 1];
+function reverteUltimoMovimento(novaCasa, possibilidades, resultado) {
+    possibilidades.push(resultado.pop());
+    novaCasa = resultado[resultado.length - 1];
 }
 
-function novoMovimento(casa, possibilidades) {
-    const jogadasAprimorado = coletaJogadasPossiveisIniciandoPelasBordas(casa, possibilidades, possibilidadesBordaTabuleiro, possibilidadesSegundaCasaApartirDaBordaTabuleiro)
-    for (let index = 0; index < jogadasAprimorado.length; index++) {
-        const jogada = jogadasAprimorado[index];
-        const novaCasa = [casa[0] + jogada[0], casa[1] + jogada[1]];
-        movimentos(novaCasa, possibilidades, result);
-        novoMovimento(novaCasa, possibilidades, result);
-        if (possibilidades.length > 0) {
-            movimentosReverso(novaCasa, possibilidades, result);
+function passeioDoCavalo(casaAtual, possibilidades) {
+    const jogadasPossiveis = coletaJogadasPossiveisDandoPreferenciaPelasComMenoresPossibilidades(casaAtual, possibilidades);
+    for (let index = 0; index < jogadasPossiveis.length; index++) {
+        const jogada = jogadasPossiveis[index];
+        const novaCasa = [casaAtual[0] + jogada[0], casaAtual[1] + jogada[1]];
+        // console.log(resultado)
+        contabilizandoMovimento(novaCasa, possibilidades, resultado);
+        passeioDoCavalo(novaCasa, possibilidades, resultado);
+        if (possibilidades.length > 2) {
+            reverteUltimoMovimento(novaCasa, possibilidades, resultado);
         } else {
             return;
         }
-
     }
 }
 
-function coletaJogadasPossiveisIniciandoPelasBordas(casa, possibilidades, possibilidadesBordaTabuleiro) {
-    const jogadas = [
+function coletaJogadasPossiveisDandoPreferenciaPelasComMenoresPossibilidades(casa, possibilidades) {
+    const movimentosPossiveisParaOCavalo = [
         [2, 1],
         [2, -1],
         [1, 2],
@@ -80,49 +104,34 @@ function coletaJogadasPossiveisIniciandoPelasBordas(casa, possibilidades, possib
         [-2, 1],
         [-2, -1]
     ];
-    let jogadasBorda = []
-    let jogadasCentro = []
-    let jogadasSegundacasaApartirdaBorda = []
-    for (let i = 0; i < jogadas.length; i++) {
-        const jogada = jogadas[i];
-        const alvoCentro = [casa[0] + jogada[0], casa[1] + jogada[1]];
-        const indexBorda = possibilidadesBordaTabuleiro.findIndex((element) => element[0] === alvoCentro[0] && element[1] === alvoCentro[1]);
-        const indexSegundaCasaApartirBorda = possibilidadesSegundaCasaApartirDaBordaTabuleiro.findIndex((element) => element[0] === alvoCentro[0] && element[1] === alvoCentro[1]);
-        const indexCentro = possibilidades.findIndex((element) => element[0] === alvoCentro[0] && element[1] === alvoCentro[1]);
-        if (indexBorda !== -1 && indexCentro !== -1) {
-            jogadasBorda.push(jogada);
-        } else if (indexSegundaCasaApartirBorda !== -1 && indexCentro !== -1)
-            jogadasSegundacasaApartirdaBorda.push(jogada)
-        if (indexBorda === -1 && indexCentro !== -1) {
-            jogadasCentro.push(jogada)
+    const possibilidadesCantos = [[1, 1], [8, 8], [1, 8], [8, 1]];
+    const possibilidadesBordaTabuleiro = coletarBordasDoTabuleiro(possibilidades, tamanhoTabuleiro, 1);
+    const possibilidadesSegundaCasaApartirDaBordaTabuleiro = coletarBordasDoTabuleiro(possibilidades, tamanhoTabuleiro, 2);
+    const movimentosQueVaoParaOsCantos = [];
+    const movimentosQueVaoParaBorda = [];
+    const MovimentosQueVaoParaSegundaCasaApartirDaBordas = [];
+    const movimentosQueVaoParaOCentro = [];
+    for (let i = 0; i < movimentosPossiveisParaOCavalo.length; i++) {
+        const movimentoDoCavalo = movimentosPossiveisParaOCavalo[i];
+        const casaAlvo = [casa[0] + movimentoDoCavalo[0], casa[1] + movimentoDoCavalo[1]];
+        const indexPossibilidadesRestantes = possibilidades.findIndex((element) => element[0] === casaAlvo[0] && element[1] === casaAlvo[1]);
+        const indexSegundaCasaApartirDaBorda = possibilidadesSegundaCasaApartirDaBordaTabuleiro.findIndex((element) => element[0] === casaAlvo[0] && element[1] === casaAlvo[1]);
+        const indexCasaDaBorda = possibilidadesBordaTabuleiro.findIndex((element) => element[0] === casaAlvo[0] && element[1] === casaAlvo[1]);
+        const indexCantos = possibilidadesCantos.findIndex((element) => element[0] === casaAlvo[0] && element[1] === casaAlvo[1]);
+        const jogadaMoveParaABordaEEstaNasPossibilidades = indexCasaDaBorda !== -1 && indexPossibilidadesRestantes !== -1;
+        const jogadaMoveParaSegundaCasaApartirDaBordaEEstaNasPossibilidades = indexSegundaCasaApartirDaBorda !== -1 && indexPossibilidadesRestantes !== -1;
+        const jogadaMoveParaOCentroEEstaNasPossibilidades = indexCasaDaBorda === -1 && indexPossibilidadesRestantes !== -1;
+        const jogadaMoveParaOCantoEEstaNasPossibilidades = indexCantos === -1 && indexPossibilidadesRestantes !== -1;
+        if (jogadaMoveParaOCantoEEstaNasPossibilidades) {
+            movimentosQueVaoParaOsCantos.push(movimentoDoCavalo);
+        } else if (jogadaMoveParaABordaEEstaNasPossibilidades) {
+            movimentosQueVaoParaBorda.push(movimentoDoCavalo);
+        } else if (jogadaMoveParaSegundaCasaApartirDaBordaEEstaNasPossibilidades) {
+            MovimentosQueVaoParaSegundaCasaApartirDaBordas.push(movimentoDoCavalo);
+        } else if (jogadaMoveParaOCentroEEstaNasPossibilidades) {
+            movimentosQueVaoParaOCentro.push(movimentoDoCavalo);
         }
     }
-    const jogadasPossiveis = jogadasBorda.concat(jogadasSegundacasaApartirdaBorda).concat(jogadasCentro)
-    return jogadasPossiveis
-}
-
-function coletarBordasDoTabuleiro(possibilidades, tamanhoTabuleiro) {
-    let bordas = []
-    for (let index = 0; index < possibilidades.length; index++) {
-        const element = possibilidades[index];
-        const coletandoDiagonalSuperior = element[0] < 2 | element[1] < 2
-        const coletandoDiagonalInferior = element[0] > tamanhoTabuleiro - 1 | element[1] > tamanhoTabuleiro - 1
-        if (coletandoDiagonalSuperior | coletandoDiagonalInferior) {
-            bordas.push(element)
-        }
-    }
-    return bordas
-}
-
-function coletarSegundaCasaApartirDaBordaDoTabuleiro(possibilidades, tamanhoTabuleiro) {
-    let bordas = []
-    for (let index = 0; index < possibilidades.length; index++) {
-        const element = possibilidades[index];
-        const coletandoDiagonalSuperior = element[0] < 3 | element[1] < 3
-        const coletandoDiagonalInferior = element[0] > tamanhoTabuleiro - 2 | element[1] > tamanhoTabuleiro - 2
-        if (coletandoDiagonalSuperior | coletandoDiagonalInferior) {
-            bordas.push(element)
-        }
-    }
-    return bordas
+    const jogadasPossiveisOrdenadasPelasPeriferias = movimentosQueVaoParaOsCantos.concat(movimentosQueVaoParaBorda).concat(MovimentosQueVaoParaSegundaCasaApartirDaBordas).concat(movimentosQueVaoParaOCentro);
+    return jogadasPossiveisOrdenadasPelasPeriferias;
 }
