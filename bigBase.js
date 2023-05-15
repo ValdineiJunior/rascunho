ab10d11a817f1178e213e3ba337f401941340c43
Valdinei de Paula Junior <valdineidepaulajunior@gmail.com>
2023-05-15 14:42:52 -0300

    desafio bigBase completo

--- bigBase.js
+++ bigBase.js
@@ -1,16 +1,31 @@
-const base_entrada = process.argv[2];
-const base_saida = process.argv[3];
-const numero_entrada = process.argv[4];
+const baseEntrada = process.argv[2];
+const baseSaida = process.argv[3];
+const numeroEntrada = process.argv[4];
 const bases = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+const basesInvalida = baseEntrada < 2 && baseSaida > 62 && baseSaida < 2 && baseSaida > 62;
+const numeroNegativo = numeroEntrada < 0
+let numeroInvalidoParaBaseExpecifica = false
+const digitosValidosParaBaseExpecifica = [...bases.slice(0,baseEntrada)]
+console.log(digitosValidosParaBaseExpecifica)
+for (let index = 0; index < numeroEntrada.length; index++) {
+  const element = numeroEntrada[index];
+  digitoPertenceABase = digitosValidosParaBaseExpecifica.findIndex((char => char === element))
+  if (digitoPertenceABase == -1) {
+    numeroInvalidoParaBaseExpecifica = true
+  }
+}
+if (basesInvalida || numeroNegativo || numeroInvalidoParaBaseExpecifica) {
+  return console.log('???')
+}
 const arrayBases = bases.split("")
 numeroNaBaseDez = 0
-expoente = numero_entrada.length - 1
-for (let index = 0; index < numero_entrada.length; index++) {
-  console.log(numero_entrada[index])
-  const element = numero_entrada[index];
+expoente = numeroEntrada.length - 1
+for (let index = 0; index < numeroEntrada.length; index++) {
+  console.log(numeroEntrada[index])
+  const element = numeroEntrada[index];
   baseElevada = 1
   for (let j = 0; j < expoente; j++) {
-    baseElevada *= base_entrada
+    baseElevada *= baseEntrada
   }
   // console.log("element: ",element,"baseElevada",baseElevada)
   elementoNaBaseDez = arrayBases.findIndex((char => char === element))
@@ -20,7 +35,11 @@ for (let index = 0; index < numero_entrada.length; index++) {
 
 let result = ''
 while (numeroNaBaseDez > 0) {
-  numeroNaBaseDez
+  resto = numeroNaBaseDez % baseSaida;
+  result = bases[resto] + result;
+  numeroNaBaseDez = (numeroNaBaseDez - resto) / baseSaida
 }
+
 console.log(numeroNaBaseDez)
+console.log(result)
 // numeroNaBaseDez = 1 * 2^3 + 0 * 2^2 + 1 * 2^1 + 1 * 2^0
