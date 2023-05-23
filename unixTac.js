6db92bd15a76e3d7077f4c4839ab7d63b86696da
Valdinei de Paula Junior <valdineidepaulajunior@gmail.com>
2023-05-23 16:08:37 -0300

    unixtac

--- unixTac.js
+++ unixTac.js
@@ -1,31 +1,29 @@
-const readline = require('readline');
 const fs = require('fs');
 
-async function tac(fileName) {
-  const fileStream = fs.createReadStream(fileName);
-  const rl = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity
-  });
-
-  let lines = [];
-  let lineCount = 0;
-
-  for await (const line of rl) {
-    lines.unshift(line);
-    lineCount++;
-
-    if (lineCount >= 10000) {
-      lines.forEach((line) => console.log(line));
-      lines = [];
-      lineCount = 0;
-    }
-  }
-
-  // Imprimir as linhas restantes
-  lines.forEach((line) => console.log(line));
+if (process.argv.length < 3) {
+  console.log('É necessário fornecer o nome do arquivo como parâmetro.');
+  console.log('Exemplo: node unixTac.js mini1GB.txt');
+  process.exit(1);
 }
 
-// Uso: node tac.js arquivo
-const fileName = process.argv[2];
-tac(fileName);
+const inputFile = process.argv[2];
+
+const stream = fs.createReadStream(inputFile, { encoding: 'utf8' });
+
+stream.on('data', (chunk) => {
+  const lines = chunk.split('\n');
+  const lastLines = lines.slice(-101);
+  const firstLines = lines.slice(0, -101);
+
+  for (let i = lastLines.length - 1; i >= 0; i--) {
+    console.log(lastLines[i]);
+  }
+
+  for (let i = firstLines.length - 1; i >= 0; i--) {
+    console.log(firstLines[i]);
+  }
+});
+
+stream.on('error', (error) => {
+  console.error('Ocorreu um erro ao ler o arquivo:', error);
+});
