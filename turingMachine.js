ad48292c96b802b6c526b1af7882c7792830fda3
Valdinei de Paula Junior <valdineidepaulajunior@gmail.com>
2023-05-25 17:10:14 -0300

    versao Inicial Primos Em Pi que coleta substring com sequencia de possiveis números primos

---
+++ turingMachine.js
@@ -0,0 +1,15 @@
+let currentState = "0";
+let currentSymbol = "0";
+const estadoInicial = "0";
+
+// casa estado mais simbolo
+const stringDeDados = "";
+
+
+const regras = [a,b,c,d,e,f];
+
+// Cada regra poder opcionalmente executar uma (ou várias) das tarefas abaixo:
+// Gravar um novo símbolo na posição corrente.
+// Mover a posição de leitura para a direita.
+// Mover a posição de leitura para a esquerda.
+// Mudar o estado atual da máquina.
