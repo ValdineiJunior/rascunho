187578372fd2d70e3d5c008b8ad760daa5bbadc0
Valdinei de Paula Junior <valdineidepaulajunior@gmail.com>
2023-06-30 17:43:30 -0300

    try solve turing

--- turning/turingMachine.js
+++ turning/turingMachine.js
@@ -32,8 +32,65 @@ function turningMachine(rules, input) {
     const { current_state, current_symbol, new_symbol, direction, new_state } = line;
     arrayRules.push([current_state, current_symbol, new_symbol, direction, new_state])
   }
-  console.table(arrayRules)
+  let tape = input.split('');
+  tape.pop()
+  let currentState = '0';
+  let tapePosition = 0;
+  // console.table(arrayRules)
+  let contador = 0
+  while(true) {
+    const currentSymbol = tape[tapePosition];
+    const [current_state, current_symbol, new_symbol, direction, new_state] = findPair(currentState,currentSymbol)
+    if (result == null) {
+      console.log(`${rules},${input},ERR`); // Regra não encontrada, imprimir "ERR"
+      return;
+    }
+    console.log(result)
+    if (result.new_state.startsWith('halt')) {
+      console.log(`${rules},${input},${tape.join('')}`); // Finalizar e imprimir a fita
+      return;
+    }
+
+    // Executar as ações da regra
+    tape[tapePosition] = result.new_symbol;
+    if (result.direction === 'r') {
+      tapePosition++;
+      if (tapePosition >= tape.length) {
+        tape.push('_'); // Adicionar espaço em branco caso a posição exceda o tamanho atual da fita
+      }
+    } else if (result.direction === 'l') {
+      tapePosition--;
+      if (tapePosition < 0) {
+        tape.unshift('_'); // Adicionar espaço em branco caso a posição seja menor que 0
+        tapePosition = 0;
+      }
+    }
+
+    currentState = result.new_state;
+
+    // console.table(result)
+    contador++
+  }
 }
+
+function findPair(state, symbol) {
+  for (let i = 0; i < arrayRules.length; i++) {
+    // console.table(arrayRules[i])
+    // console.log("estado rules:",arrayRules[i][1])
+    // console.log("estado",state)
+    if (arrayRules[i][0] === state && arrayRules[i][1] === symbol) {
+      return arrayRules[i];
+    }
+  }
+
+  for (let i = 0; i < arrayRules.length; i++) {
+    if ((arrayRules[i][1] === state || arrayRules[i][1] === '*') && (arrayRules[i][2] === symbol || arrayRules[i][2] === '*')) {
+      return arrayRules[i];
+    }
+  }
+
+  return null; // Pair not found
+};
 
 const filename = process.argv[2];
 
